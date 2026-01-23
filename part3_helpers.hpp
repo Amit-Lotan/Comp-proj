30a1139f4a53b62302f557d7ed96616e4bb99f62
‪Amit Lotan‬‏ <129232438+Amit-Lotan@users.noreply.github.com>
2026-01-24 01:14:01 +0200

    Update part3_helpers.hpp

--- part3_helpers.hpp
+++ part3_helpers.hpp
@@ -8,50 +8,37 @@
 
 using namespace std;
 
-// Error codes
 #define LEXICAL_ERROR     1
 #define SYNTAX_ERROR      2
 #define SEMANTIC_ERROR    3
 #define OPERATIONAL_ERROR 4
 
-// Types
 enum Type {
     void_t,
     int_t,
-    float_type // Renamed from float_t to avoid <cmath> collision
+    float_type
 };
 
-// Semantic value struct (Node)
 struct Node {
     string str;
     Type type = void_t;
-    
-    // Register number allocated for this expression
     int regNum = -1;
-    
-    // Offset in stack (for variables)
     int offset = 0;
-    
-    // Quad number (for markers)
     int quad = 0;
     
-    // Backpatching lists
     vector<int> nextList;
     vector<int> trueList;
     vector<int> falseList;
 
-    // For declarations / function parameters
     vector<string> paramNames;
     vector<Type> paramTypes;
-    vector<int> paramRegs; // For call arguments
+    vector<int> paramRegs;
     
-    // For named arguments
     vector<string> namedLabels;
     vector<int> namedRegs;
     vector<Type> namedTypes;
 };
 
-// Global Helper Classes
 class CodeBuffer {
 public:
     CodeBuffer();
@@ -68,7 +55,7 @@ private:
 struct Function {
     Type returnType;
     vector<Type> paramTypes;
-    vector<string> paramNames; // Needed for named args
+    vector<string> paramNames;
     int startLineImplementation;
     bool defined;
     vector<int> callingLines;
@@ -79,7 +66,6 @@ struct Symbol {
     Type type;
 };
 
-// Helpers
 string intToString(int i);
 template <typename T>
 vector<T> merge(const vector<T>& l1, const vector<T>& l2) {

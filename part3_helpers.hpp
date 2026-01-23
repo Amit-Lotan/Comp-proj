80263e9bfe0c3df7449aaec7b66e6bdb136364a8
‪Amit Lotan‬‏ <129232438+Amit-Lotan@users.noreply.github.com>
2026-01-24 00:52:24 +0200

    Refactor part3_helpers.hpp structure and definitions
    Refactor header guards, reorganize includes, and update type definitions.

--- part3_helpers.hpp
+++ part3_helpers.hpp
@@ -1,112 +1,91 @@
-#ifndef _PART3_HELPER_H_
-#define _PART3_HELPER_H_
+#ifndef PART3_HELPERS_HPP
+#define PART3_HELPERS_HPP
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <vector>
 #include <string>
-#include <sstream>
-#include <algorithm>
+#include <vector>
 #include <map>
-
-#define LEXICAL_ERROR 1
-#define SYNTAX_ERROR 2
-#define SEMANTIC_ERROR 3
-#define OPERATIONAL_ERROR 9
+#include <iostream>
 
 using namespace std;
 
-typedef enum {int8 = 8, int16 = 16, int32 = 32, void_t = 0} Type;
+// Error codes
+#define LEXICAL_ERROR     1
+#define SYNTAX_ERROR      2
+#define SEMANTIC_ERROR    3
+#define OPERATIONAL_ERROR 4
 
-// Generate a string out of an int
-static string intToString(double num) {
-    stringstream ss;
-    ss << num;
-    return ss.str();
-}
-// Merge two lists for backpatching
+// Types
+enum Type {
+    void_t,
+    int_t,
+    float_type // Renamed from float_t to avoid <cmath> collision
+};
+
+// Semantic value struct (Node)
+struct Node {
+    string str;
+    Type type = void_t;
+    
+    // Register number allocated for this expression
+    int regNum = -1;
+    
+    // Offset in stack (for variables)
+    int offset = 0;
+    
+    // Quad number (for markers)
+    int quad = 0;
+    
+    // Backpatching lists
+    vector<int> nextList;
+    vector<int> trueList;
+    vector<int> falseList;
+
+    // For declarations / function parameters
+    vector<string> paramNames;
+    vector<Type> paramTypes;
+    vector<int> paramRegs; // For call arguments
+    
+    // For named arguments
+    vector<string> namedLabels;
+    vector<int> namedRegs;
+    vector<Type> namedTypes;
+};
+
+// Global Helper Classes
+class CodeBuffer {
+public:
+    CodeBuffer();
+    void emit(const string& str);
+    void emit_front(const string& str);
+    void backpatch(const vector<int>& lst, int line);
+    int nextquad();
+    string printBuffer();
+
+private:
+    vector<string> data;
+};
+
+struct Function {
+    Type returnType;
+    vector<Type> paramTypes;
+    vector<string> paramNames; // Needed for named args
+    int startLineImplementation;
+    bool defined;
+    vector<int> callingLines;
+};
+
+struct Symbol {
+    int offset;
+    Type type;
+};
+
+// Helpers
+string intToString(int i);
 template <typename T>
-static vector<T> merge(vector<T>& lst1, vector<T>& lst2) {
-	vector<T> result = lst1;
-	result.insert(result.end(), lst2.begin(), lst2.end());
-	return result;
+vector<T> merge(const vector<T>& l1, const vector<T>& l2) {
+    vector<T> ret = l1;
+    ret.insert(ret.end(), l2.begin(), l2.end());
+    return ret;
 }
-// Definition of a token's fields
-typedef struct {
-	// Name of the token (in case it represents an identifier)
-	string str;
-	// Type of the token
-	Type type;
-	// Offset in memory
-	int offset;
-	vector<int> nextList; 
-	vector<int> trueList;
-	vector<int> falseList;
-	// Number of line the token's expression will be printed at in the .rsk file
-	int quad;
-	// Number of the register the token is assigned
-	int regNum;
-	// List of parameters' types in function's defenition
-	vector<Type> paramTypes;
-	// List of parameters registers' numbers in function's defenition
-	vector<int>  paramRegs; 
-} yystype;
-// Definition of Symbol struct
-class Symbol {
-	public:
-		map<int,Type> type; //each element holds the depth of the symbol and its type
-		map<int,int> offset;// first int is the depth, second is 
-		int depth; //the depth in which the most inner symbol is defined in.
-};
-// Definition of Function class
-class Function {
-	public:
-		// The starting address in the buffer of the function implementation
-		int startLineImplementation; 
-		// The return type of the function.  
-		Type returnType;
-		// The types of parameters of the function
-		vector<Type> paramTypes; 
-		// List of all the addresses in .rsk file where the function is being called
-		vector<int> callingLines;
-		// Indicator for if the function has been defined or not
-		bool defined; 
-};
-
-
-// Definition of Buffer class
-class Buffer {
-		vector<string> data;
-	public:
-		Buffer();
-		// Emit a command as a new line at the end of the buffer
-		void emit(const string& str);
-		// Emit a command as a new line at the start of the buffer
-		void emit_front(const string& str);
-		// Backpatch the holes in list with a given line number
-		void backpatch(vector<int> lst, int line);
-		// Return the next empty line in the buffer
-		int nextquad();
-		// Print all the data in the buffer 
-		string printBuffer();
-};
-
-
-// Global variables
-static Buffer * buffer; 
-static Buffer mainBuffer;
-static map<string, Symbol> symbolTable; 
-static map<string, Function> functionTable;
-static int currentReturnType;
-static int currentScopeRegsNum = 3; // Number of registers in use in the current function
-static int currentScopeOffset = 0; // Current offset in memory: where to store the next piece of data
-static int prevScopeOffset = 0;  
-static int currentBlockDepth = 0;  // Current depth in blocks relatively to the most outer block
-static vector<string> currentParamInsertionOrder;
-static vector<string> tmpParamInsertionOrder;
-
-#define YYSTYPE yystype
 
 #endif
